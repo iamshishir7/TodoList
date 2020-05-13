f914523e5d6397e142cd68239be0c95f203b0580
Shishir Timalsina <62695104+iamshishir7@users.noreply.github.com>
2020-05-13 22:09:34 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const addtodo = document.querySelector('.add');
+const searchtodo = document.querySelector('.search input');
+const todolists = document.querySelector ('ul')
+
+
+const newtemplate = (newtodo) => {
+    html = `
+            <li>
+                <span>${newtodo}</span>
+                <i class="far fa-trash-alt delete"></i>
+            </li>
+            `
+    todolists.innerHTML += html;
+}
+
+addtodo.addEventListener('submit',e => {
+    e.preventDefault();
+    const newtodo = addtodo.add.value.trim();
+    if (newtodo.length) {
+        newtemplate(newtodo);
+        addtodo.reset();
+    }
+});
+
+todolists.addEventListener('click',e => {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+});
+
+const searching = (term) => {
+    Array.from(todolists.children)
+    .filter(todo => !todo.textContent.includes(term))
+    .forEach(todo => todo.classList.add('donotdisplay'));
+
+    Array.from(todolists.children)
+    .filter(todo => todo.textContent.includes(term))
+    .forEach(todo => todo.classList.remove('donotdisplay'));
+
+    
+}
+
+searchtodo.addEventListener ('keyup', () => {
+    const term = searchtodo.value.trim();
+    searching(term);
+});
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+})
+
